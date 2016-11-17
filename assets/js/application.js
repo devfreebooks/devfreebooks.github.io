a7e766d14cb20ec59fddc0a3e3493211c0a102a3
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2016-11-17 19:46:47 -0200

    Deploying DevFreeBooks

--- assets/js/application.js
+++ assets/js/application.js
@@ -16,13 +16,38 @@
       var anchor = document.querySelector(window.location.hash);
       anchor.nextElementSibling.classList.add('featured');
     }
-    var newBooks = document.querySelectorAll('[data-added]');
-    for (var i = 0, max = newBooks.length; i < max; i++) {
+    var tags = document.querySelectorAll('[data-tags]');
+    var filters = document.querySelectorAll('[data-filter]');
+    var filterList = [].slice
+      .call(filters)
+      .map(function(f) { return f.getAttribute('data-filter'); })
+      .filter(function(f) { return f !== 'all'; })
+    ;
+    filters.forEach(function(filterEl) {
+      filterEl.addEventListener('click', function(e) {
+        var filterType = e.currentTarget.getAttribute('data-filter');
+        tags.forEach(function(tagEl) {
+          var tagList = tagEl.getAttribute('data-tags').split(',');
+          tagEl.classList.add('hide');
+          if (filterType === 'all') {
+            tagEl.classList.remove('hide');
+            return true;
+          }
+          filterList.forEach(function(filterValue) {
+            if (filterType === filterValue && tagList.indexOf(filterValue) !== -1) {
+              tagEl.classList.remove('hide');
+            }
+          });
+        });
+      });
+    });
+    var addedList = document.querySelectorAll('[data-added]');
+    addedList.forEach(function(added) {
       var todayDate = +getFormatedDateOfToday();
-      var bookDate = +newBooks[i].getAttribute('data-added');
-      if (todayDate - bookDate <= 10) {
-        newBooks[i].classList.remove('hide');
+      var addedDate = +added.getAttribute('data-added');
+      if (todayDate - addedDate <= 10) {
+        added.classList.remove('hide');
       }
-    }
+    });
   });
 })();
