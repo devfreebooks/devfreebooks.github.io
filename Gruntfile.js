cf36397fbc417e7cce23bf8daea49fc97a78da7e
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2018-06-27 11:51:21 -0300

    update some books

--- Gruntfile.js
+++ Gruntfile.js
@@ -49,26 +49,6 @@ module.exports = (grunt) => {
         command: 'node categories.js'
       }
     },
-    // Manifest ======================================
-    appcache: {
-      options: {
-        basePath: 'www',
-        baseUrl: harp.globals.root_url.production
-      },
-      all: {
-        dest: 'www/manifest.appcache',
-        cache: {
-          patterns: [
-            'www/404.html',
-            'www/manifest.json',
-            'www/robots.txt',
-            'www/favicon.ico',
-            'www/assets/**/*.{css,js,woff,ttf,svg,eot,gif,png,jpg,jpeg}'
-          ]
-        },
-        network: '*'
-      }
-    },
     // Sitemap =======================================
     sitemaps: {
       default: {
@@ -82,25 +62,6 @@ module.exports = (grunt) => {
           cwd: 'www/',
           src: '**/*.html'
         }]
-      }
-    },
-    // SW PreCache ===================================
-    'sw-precache': {
-      options: {
-        baseDir: 'www',
-        cacheId: harp.globals.slug_title,
-        workerFileName: 'sw.js',
-        verbose: true,
-        handleFetch: true
-      },
-      default: {
-        staticFileGlobs: [
-          '404.html',
-          'manifest.json',
-          'robots.txt',
-          'favicon.ico',
-          'assets/**/*.{css,js,woff,ttf,svg,eot,gif,png,jpg,jpeg}'
-        ]
       }
     },
     // Github Pages ==================================
@@ -121,14 +82,13 @@ module.exports = (grunt) => {
   grunt.loadNpmTasks('grunt-contrib-compress');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
-  grunt.loadNpmTasks('grunt-appcache');
-  grunt.loadNpmTasks('grunt-sw-precache');
+
   grunt.loadNpmTasks('grunt-sitemaps');
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-newer');
 
   grunt.registerTask('prebuild:dev', ['clean', 'env:dev', 'shell:categories', 'newer:imagemin']);
   grunt.registerTask('prebuild:prod', ['clean', 'env:prod', 'shell:categories', 'newer:imagemin']);
-  grunt.registerTask('build:prod', ['shell:feed', 'appcache', 'sw-precache', 'compress', 'sitemaps']);
+  grunt.registerTask('build:prod', ['shell:feed', 'compress', 'sitemaps']);
   grunt.registerTask('deploy:prod', ['gh-pages', 'clean']);
 };
