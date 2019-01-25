76764536ce700481a1463e066100ee132ad7faf4
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2019-01-25 21:11:19 -0200

    fix feed

--- feed.js
+++ feed.js
@@ -1,9 +1,10 @@
-const harp = require('./harp.json').globals;
 const RSS = require('rss');
 const moment = require('moment');
 const fs = require('fs-extra');
 const path = require('path');
+const { globals } = require('./harp.json');
 
+const harp = globals;
 const rootUrl = harp.root_url.production;
 const categoryDir = path.join(__dirname, 'public/_categories');
 const imageRootUrl = `${rootUrl}assets/images/`;
@@ -26,20 +27,15 @@ fs.readdirSync(categoryDir).forEach((categoryFile) => {
   category.books.forEach((book) => {
     const bookId = book.title.replace(/[^\w\s]/g, '').replace(/\s/g, '-').toLowerCase();
     const bookLink = `${rootUrl}${categoryName}/#${bookId}`;
-    const bookImage = book.image || `${categoryName}.png`;
     const bookPublishedAt = moment(book.added_at || moment().format('YYYYMMDD'), 'YYYYMMDD');
     books.push({
       title: `${book.paid_book ? 'Sponsored' : 'Free'} book: ${book.title}`,
       description: `
         <article>
-          <figure style="text-align:center;">
-            <img src="${imageRootUrl}${bookImage}" title="${book.title}" />
-            <figcaption>${book.title}</figcaption>
-          </figure>
           <p>
             ${book.description}
             <br />
-            Author: <b>${book.author}</b> | Section: <b>${category.subtitle}</b>
+            Author: <b>${book.author}</b> | Category: <b>${category.subtitle}</b>
             <br />
             Lang: <b>${book.lang}</b> | Pages: <b>${book.pages}</b> | Year: <b>${book.year}</b>
           </p>
